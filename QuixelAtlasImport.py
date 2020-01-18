5e9009a3ad207d84557448847cc2274efe410c91
Tilman <tm.selsingen@gmail.com>
2020-01-18 04:02:09 +0100

    Initial commit

---
+++ QuixelAtlasImport.py
@@ -0,0 +1,13 @@
+##Initialize
+#Global Variables
+
+#LibraryPath = PathDummy 
+#SourcePath = sPathDummy
+#ID = IDdummy
+#MainCat = MainCatDummy
+#SecCat = SecCatDummy
+#Name = NameDummy #für Bridge
+#Resolution = ResDummy
+#ScanArea = ScanAreaDummy
+#tags = []
+# ""/'' can be used interchangably but the other has to be used if one is part of the string that is getting defined.
