4c24682aef16b9b218e28d24b670ee1d4a710ff8
Tilman <tm.selsingen@gmail.com>
2020-01-20 08:16:38 +0100

    Working, tested.
    All as intended.

--- QuixelAtlasImport.py
+++ QuixelAtlasImport.py
@@ -31,61 +31,72 @@
 # ##### END GPL LICENSE BLOCK #####
 
 # 1. Promt User for Library Path
-import os
-import re
-import PIL
-import json
-
+import os, re, PIL, json, fileinput, sys
+from PIL import Image
+from shutil import copyfile
 #from __future__ import print_function ##to get access to line sep features. print("Foo", "bar", sep= "tauscht das Komma mit Inhalt der Anführungszeichen aus")
-
+prevcheck=0
 while True: #envelope loop
 	##Input Assistant:
 	print("be careful that you enter following inputs correctly, there is no check for invalid paths. The script is not case sensitive. \n")
-	LibraryPath = raw_input("Enter the path of your Quixel Library Directory (e.g. C:\Data\Quixel Library) \n")+"\\"
+	LibraryPath = input("Enter the path of your Quixel Library Directory (e.g. C:\\Data\\Quixel Library) \n")+"\\"
 	print("Caution. The script identifies the maps according to matching the first three letters of each name (except two for AO). ALBedo, AO, DISplacement, GLOss, NORmal, OPAcity ROUghness, SPEcular, TRAnslucency. As long these three letters are in the respective file name, the script detects them: \n ")
-	SourcePath = raw_input("Enter the path of your source folder containing all texture maps to be imported (e.g. C:\Users\You\Documents\MyAwesomeDecal). If you have a custom preview as png(!), put it in this folder named 'preview.png', to make the script use this instead of a generated one. It will be automatically resizied to 1280x1280/360x360.\n ")+"\\"
-	name = raw_input("Enter the name you wish to see in Bridge after import")
-	ID = raw_input("Enter the ID you wish to have for your Atlas/Decal. (Tip: use the ID of the source atlas with xy coordinates describing the position of the Decal in the Atlas in terms of rows and columns. rcihc2 ->xyrcihc2 e.g. 24rcihc2): \n ")
-	mCat = raw_input("Enter the Top-Level Category for your Atlas/Decal, e.g. Asphalt, Brick, Brushes, Bush, Climber,... .: ")
-	sCat = raw_input("Enter the secondary Category for your Atlas/Decal, e.g. for Top Level Asphalt - Coarse, Cracked, Dried, Fine, ...: \n")
+	SourcePath = input("Enter the path of your source folder containing all texture maps to be imported (e.g. C:\\Users\You\\Documents\\MyAwesomeDecal). If you have a custom preview as png(!), put it in this folder named 'preview.png', to make the script use this instead of a generated one. It will be automatically resizied to 1280x1280/360x360.\n")+"\\"
+	name = input("Enter the name you wish to see in Bridge after import:\n")
+	ID = input("Enter the ID you wish to have for your Atlas/Decal. (Tip: use the ID of the source atlas with xy coordinates describing the position of the Decal in the Atlas in terms of rows and columns. rcihc2 ->xyrcihc2 e.g. 24rcihc2): \n")
+	IDminus2 = ID[:-1]
+	mCat = input("Enter the Top-Level Category for your Atlas/Decal, e.g. Asphalt, Brick, Brushes, Bush, Climber,... .:\n")
+	sCat = input("Enter the secondary Category for your Atlas/Decal, e.g. for Top Level Asphalt - Coarse, Cracked, Dried, Fine, ...: \n")
 	
 	## Resolution Chooser. 1,2,4,8 und enter um Auflösung zu wählen.
-	resChoose = raw_input("For 1K enter 1, for 2K enter 2, for 4K enter 4, for 8K enter 8.")
-	if resChoose == 1:
-		print "You entered 1K."
+	resChoose = input("For 1K enter 1, for 2K enter 2, for 4K enter 4, for 8K enter 8.\n")
+	if resChoose == "1":
+		print ("You entered 1K.")
 		resolution = "1K"
-	elif resChoose == 2:
+	elif resChoose == "2":
 		print("You entered 2K.")
 		resolution = "2K"
-	elif resChoose == 4:
+	elif resChoose == "4":
 		print("You entered 4K.")
 		resolution = "4K"
-	elif resChoose == 8:
+	elif resChoose == "8":
 		print("You entered 8K.")
 		resolution = "8K"
 	else :
 		print("Please enter a valid number.")
 	##MetaData
-	scanAreaVar = raw_input("Enter the dimensions of the scanned area. This is meta-data and not vital information. e.g. 2x2, 0.25x0.25 etc. Formatting is important.")
-	height = raw_input("Enter height of the scanned surface. This is meta-data and not vital information.")
+	scanAreaVar = input("Enter the dimensions of the scanned area. This is meta-data and not vital information. e.g. 2x2, 0.25x0.25 etc. Formatting is important.\n")
+	heightVar = input("Enter height of the scanned surface. This is meta-data and not vital information.\n")
 	
 	##Enter Tags:
-	tags = []
-	 while True:
-		tmptag = raw_input("You can now enter tags, one at a time, and press enter to accept it. If you are done, enter 'done' to continue with the script. \n")
+	tagsIn = []
+	while True:
+		tmptag = input("You can now enter tags, one at a time, and press enter to accept it. If you are done, enter 'done' to continue with the script. \n")
 		if tmptag != 'done':
-			tags.append(tmptag)
+			tagsIn.append(tmptag)
 		else:
 			break
-	print("Here you can check if you made any mistakes, and if necessary, restart the assistent.","", "Libary Path: " + LibraryPath, "Source Path: " + SourcePath, "Name: " + name, "ID: " + ID, "Main Category: " + mCat, "Secondary Category: " + sCat, "Resolution: " + resolution, "Scan Area: " + scanArea, "Scan Heigt: " + height, "Tags: " + tags, sep='\n', end='Press Enter to continue...\n')
-	raw_input()#holds the program until enter is pressed.
+	print("Here you can check if you made any mistakes, and if necessary, restart the assistent.")
+	print("Libary Path: " + LibraryPath)
+	print("Source Path: " + SourcePath)
+	print("Name: " + name)
+	print("ID: " + ID)
+	print("Main Category: " + mCat)
+	print("Secondary Category: " + sCat)
+	print("Resolution: " + resolution)
+	print("Scan Area: " + scanAreaVar)
+	print("Scan Heigt: " + heightVar)
+	print("Tags: ")
+	print(tagsIn)
+	print("Press Enter to continue or re-run the script if you find an error")
+	input()#holds the program until enter is pressed.
 	##restart assistant logic:
-	while True: ##this loop assures that the user has to enter the answer again if it was invalid, and gets a break if a valid answer was given.
-	ans = raw_input("Continue? (Y/N): ") ##enter answer
-		if ans in ("y","Y","n","N"):
-			break ## wurde die korrekte Anwort gegeben, wird der while-loop beendet, und das Programm wird fortgesetzt.
-		else:
-			print("Enter a valid response") # Ist ans etwas anders ans n N y Y, wird der while Loop wiederholt. Solange bis eine korrekte Anwort gegeben wurde.
+	#while True: ##this loop assures that the user has to enter the answer again if it was invalid, and gets a break if a valid answer was given.
+	#	ans = input("Continue? (Y/N): ") ##enter answer
+	#	if ans in ("y","Y","n","N"):
+	#		break ## wurde die korrekte Anwort gegeben, wird der while-loop beendet, und das Programm wird fortgesetzt.
+	#	else:
+	#		print("Enter a valid response") # Ist ans etwas anders ans n N y Y, wird der while Loop wiederholt. Solange bis eine korrekte Anwort gegeben wurde.
 	### MAIN PROGRAM ###
 	
 	# 1. Ordner anlegen
@@ -105,51 +116,57 @@ while True: #envelope loop
 		os.mkdir(rootDir+"Thumbs\\1K")
 		os.mkdir(rootDir+"Thumbs\\2K")
 	except:
-		raw_input("Failed to create directories. Press Enter to restart assistent. ")
-		continue ##restarts the envelope loop
+		input("Failed to create directories. Check your permissions and rerun assistant.")
+		#continue ##restarts the envelope loop
 	else: #continue with rest of program
-	#read source files
+	#read & save source files
 		mapAbr = ["alb","AO","dis","glo","nor","opa","rou","spe","tra"] # patterns for regex
 		mapNames = ["Albedo","AO","Displacement","Gloss","Normal","Opacity","Roughness","Specular","Translucency"] # actual names in same order. mapAbr[0]="alb", mapNames[0]= "Albedo"
 		SourceNames = os.listdir(SourcePath) # generates array with filenames within the given directory. Isn't necessarily as long as the map-Arrays.
 		filenames=[]
 		for SourceName in SourceNames:
-			#fileName, fileExtension = os.path.splitext(SourcePath+SourceName) obsolete since Image.open opens all images and can convert all types to all types. So no need to check for jpg necessary anymore.
+			for i, item in enumerate(mapAbr):
+				if re.search(item, SourceName, flags= re.I):
+					newName=IDminus2+"_"+resolution+"_"+mapNames[i]+".jpg"
+					img = Image.open(SourcePath+SourceName)
+					img.save(rootDir+newName)
+			filenames.append(newName)
+			if re.search("preview.png", SourceName, flags=re.I):
+				preview = Image.open(SourcePath+SourceName)
+				print("Preview File found\n")
+				prevcheck=1
+		if prevcheck == 0:
+			print("No Preview found. One will be generated instead")
 			
-			for i, item in enumerate(MapAbr): #check each abbrivation and if one matches do body of if
-				if re.match(item, SourceName, flags= re.I): # if item in MapAbr (e.g. "alb") matches with SourceName in Sourcenames, execute body.
-					newName=ID+"_"+resolution+"_"+mapNames[i]+".jpg" ## generates the new name for the file based on Type, Resolution and ID.
-					img = Image.open(SourcePath+"\\"+SourceName) ##reads in image format independently
-					img.save(rootDir+newName)##saves that image with appropriate name and format
-					filenames.append(newName+"\n")##fills array with created file names to give user feedback.
-				elif re.match(preview.png, SourceName, flags=re.I): #check if a file of the source dir is named previews.png
-					preview = Image.open(SourcePath+SourceName)
-					print("Preview File found\n")
-						
-				else:
-					print(SourceName+" could't be intentified")
-		print("The following files were created based on source input:\n"+filenames
+	
+		print("The following maps were created based on source input:\n")
+		print(filenames)
 		
 		##Previews
 		# check if preview was given
-		if preview:
-			preview.resize((1280,1280),PIL.Image.ANTIALIAS)
-			preview.save(rootDir+"previews\\"+ID+"_Preview_Retina_sp.jpg")
-			preview.resize((360,360),PIL.Image.ANTIALIAS)
-			preview.save(rootDir+ID+"_Preview.png")
+		if prevcheck == 1:
+			prev1280 = preview.resize((1280,1280),PIL.Image.ANTIALIAS)
+			prev1280.save(rootDir+"previews\\"+IDminus2+"_Preview_Retina_sp.jpg")
+			prev360 = preview.resize((360,360),PIL.Image.ANTIALIAS)
+			prev360.save(rootDir+IDminus2+"_Preview.png")
 		else:
 			#1. generate Preview
-			alpha = Image.new("RGBA",(1280,1280),(0,0,0,1)) #1.1 generate Alpha-Layer
-			alb = Image.open(rootDir+ID+"_"+resolution+"_Albedo.jpg")#1.2 load albedo
-			AO = Image.open(rootDir+ID+"_"+resolution+"_AO.jpg") #1.3 load AO
-			mask = Image.open(rootDir+ID+"_"+resolution+"_Opacity.jpg") #1.4 Load Opacity Mask
+			alb = Image.open(rootDir+IDminus2+"_"+resolution+"_Albedo.jpg").convert("RGBA")#1.2 load albedo
+			AO = Image.open(rootDir+IDminus2+"_"+resolution+"_AO.jpg").convert("RGBA") #1.3 load AO
+			mask = Image.open(rootDir+IDminus2+"_"+resolution+"_Opacity.jpg").convert("L") #1.4 Load Opacity Mask
 			blended = PIL.ImageChops.multiply(alb, AO)#1.4 Multiply AO with Albedo to get somewhat representative result
-			resized = blended.resize((1280,1280),PIL.Image.ANTIALIAS)#1.5resize for 1280x1280 preview in previews dir
-			masked = Image.composite(blended, alpha, mask) # 1.6 create alpha masked image
-			masked.save(rootDir+"previews\\"+ID+"_Preview_Retina_sp.jpg")#save as jpeg in previews dir
-			masked.resize((360,360), PIL.Image.ANTIALIAS)#resize for 36x360 png preview
-			masked.save(rootDir+ID+"_Preview.png")# save 360 preview
-			print("The previews were generated by blending Albedo and AO with multiply. They are not representative of how the Atlas looks in a PBR engine.\n")
+			
+			#create 360x360 alpha masked png
+			alpha = blended.copy()
+			alpha.putalpha(mask) # uses Opacity mask to create png preview
+			alpha = alpha.resize((360,360))
+			alpha.save(rootDir+ID+"_Preview.png")
+			#create 1280x1280 JPG
+			grey = Image.new("RGB",blended.size, (34,34,34))
+			RGB = Image.composite(blended,grey,mask).resize((1280,1280),PIL.Image.ANTIALIAS)
+			RGB.save(rootDir+"previews\\"+ID+"_Preview_Retina_sp.jpg")
+			
+			print("The previews were generated by multiplying Albedo with AO. They are not representative of how the Atlas looks in a PBR engine.\n")
 		
 		##Thumbs
 		#Load Images
@@ -166,23 +183,70 @@ while True: #envelope loop
 		#create dict with key = image, resize them and save them accordingly
 		maps={}
 		for i,item in enumerate(mapAbr):
-			maps[item]= Image.open(rootDir+ID+"_"+resolution+"_"+mapNames[i]+".jpg")
+			maps[item]= Image.open(rootDir+IDminus2+"_"+resolution+"_"+mapNames[i]+".jpg")
 			maps[item]= maps[item].resize((2048,2048),PIL.Image.ANTIALIAS)
-			maps[item].save(rootDir+"thumbs\\2K\\"+mapNames[i]+".jpg")
+			maps[item].save(rootDir+"thumbs\\2K\\"+IDminus2+"_2K_"+mapNames[i]+".jpg")
 			maps[item]= maps[item].resize((1024,1024),PIL.Image.ANTIALIAS)
-			maps[item].save(rootDir+"thumbs\\1K\\"+mapNames[i]+".jpg")
+			maps[item].save(rootDir+"thumbs\\1K\\"+IDminus2+"_1K_"+mapNames[i]+".jpg")
 			
 		#JSON Editing
 		
-		#1. read json template from home-dir of script
-		#2. replace "tmpID" with ID
-		#3. replace "tmp_Name" with Name
-		#4.	replace "mCat" with mCat
-		#5.	replace "sCat" with sCat
-		#6 replace "scanAreaVar" with scanArea
-		#7 replace "heightVar" with height
+		#1. copy template json to rootDir
+		templatepath = os.path.join(sys.path[0], "template.JSON")
+		copyfile(templatepath, rootDir+ID+".json")
+		#2.replace alll "ID". The ID for atlasses has a "2" added after the ID for the name of the JSON file, as well as the directory name. The maps miss this appendix.
+		#with line.replace every part of a string matching the pattern-string gets replaced. Since it's "id:tmpID2" in the file, just the tmpID portion gets replaced.
+		
+		
 				
-		# add tags with json 
+		#Serialize JSON
+		#the rest of the data in the JSON has to be replaced with regex to avoid unwanted replacements. (Regex supports whole-word search and replace)
+		with fileinput.FileInput(os.path.join(rootDir, ID+".json"), inplace=True, backup =".bak") as f:
+			for line in f:
+				print(line.replace("tmpID", IDminus2), end='')
+		
+		with open(os.path.join(rootDir, ID+".json"), "r") as J: # opens .json as J
+			match={r"\btmpName\b":name,r"\bmCat\b":mCat,r"\bsCat\b":sCat,r"\bscanAreaVar\b":scanAreaVar,r"\bheightVar\b":heightVar}#{pattern:replacestring}
+			def replace_all(text,dic):
+				for key, val in dic.items():
+					text = re.sub(key,val,text)
+				return text
+			
+			
+			jdict = json.load(J) # serializes the content of the loaded json as "jcontent" (type dict)
+			jdict['tags'] = tagsIn
+			jstring = json.dumps(jdict, indent=4) # dumps the content into a single string
+			jstring = replace_all(jstring,match) #replaces all keyWords in the jstring with the Variable Values
+			#jredict = json.loads(jstring) # isn't necessary.
+			
+			##tags
+			
+			
+			
+			
+			with open(os.path.join(rootDir, ID+".json"),"w") as savefile:
+				#json.dump(jredict, savefile)#dumps the modified string back into the json file.
+				savefile.write(jstring)#Writes the human-readable string to file
+			
+			
+			
+			
+		###tags	
+			
+		
+		
+		
+		with open(os.path.join(rootDir, ID+".json"), "w") as J: # opens json file as json
+			data = json.load(J) #serializes file content into "data" The whole JSON is a dict containing other dicts, mere keys:value pairs and lists/arrays
+			tags.append(tagsIn) # appends the content of tagsIn[] (the user input tags) to the "tags" array of the json serialization
+			final = json.dumps(data)
+			J.write(final)
+		
+		#Done?
+		break	
+			
+		
+		# add tags-array with json 
 		
 		#copy and rename template.json into rootDir
 		#print("Import complete. The atlas should now show up under the "mixer" category")
